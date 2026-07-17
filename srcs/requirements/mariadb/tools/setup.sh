37c7c8b9b1bb0a252ef10f26dc7e1520341fcfe8
VMinception <aandreo.4202@gmail.com>
2026-07-17 10:59:33 +0200

    Fixed few errors on script

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -10,28 +10,28 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo ">>> Premier demarrage: initialisation"
     chown -R mysql:mysql /var/lib/mysql # permet de s assurer que le dosser /var/lib/mysql appartient bien a l user mysql
     mariadb-install-db --datadir=/var/lib/mysql --user mysql --basedir=/usr --skip-test-db
-    cat << EOF > /tmp/init_sql
-    CREATE DATABASE IF NOT EXITS \'$DB_NAME'\ ;
+    cat << EOF > /tmp/init.sql
+    CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` ;
 
     /* '%' autorise la connexion reseau -> pour que ca marche entre containers */
     CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWD' ;
 
     /* donne tout les droits sur cette db uniquement */
-    GRANTS ALL PRIVILEGES ON \'$DB_NAME\'.* TO '$DB_USER'@'%' ;
+    GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'%' ;
 
     /* root en localhost pour que aucun autre container puisse acceder au root via le reseau -> securite */
     ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWD' ;
 
     /*recharge les privileges */
     FLUSH PRIVILEGES ;
-    EOF
+EOF
 
     mysql --user=mysql --bootstrap < /tmp/init.sql
-    rf -f /tmp/init.sql
+    rm -f /tmp/init.sql
 else
     echo "Database deja initialisee"
 fi
 
-/* remplace PID = 1 par mariadb
-mysqld --user=mysql
+# remplace PID = 1 par mariadb
+exec mysqld --user=mysql
 
